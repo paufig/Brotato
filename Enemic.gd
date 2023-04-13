e9d41edb0e0ef4b13248230ce04702c213a3b59a
Mightydraco <101245704+Mightydraco@users.noreply.github.com>
2023-04-13 09:41:28 +0200

    principi mov

--- Enemic.gd
+++ Enemic.gd
@@ -1,11 +1,16 @@
 extends KinematicBody2D
 
+var speed = 100
+var target = null
+var ray = null
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
+func physics_process(delta):
+	look_at(target.global_position)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
