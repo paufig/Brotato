f04d4d36fc79e518a1075e1607725a1b8c3a9245
Mightydraco <101245704+Mightydraco@users.noreply.github.com>
2023-04-27 09:08:55 +0200

    casi tot fet

--- Enemic_babosa.gd
+++ Enemic_babosa.gd
@@ -4,16 +4,18 @@ onready var knackles = get_tree().get_root().get_node("Node2D/knackles")
 var move = Vector2()
 var vel = 40
 var vida = 75
+
 func _physics_process(delta):
 	move = position.direction_to(knackles.global_position) 
 	var dir = knackles.position - position
 	move = move_and_slide(move * vel)
 	
-	if vida == 0:
-		position = Vector2(1000,1000)
+	
 
 
 func _on_Area2D_area_entered(area):
 	if area.name == "Area2D":
 		vida = vida - 25
-	print(area)  
+		area.get_parent().get_parent().queue_free()
+	if vida <= 0:
+			queue_free()
--- mapa.gd
+++ mapa.gd
@@ -1,13 +1,38 @@
 extends Node2D
 
-
-func _ready():
-	var rand = RandomNumberGenerator.new()
-	var enemyscene = load("res://Enemic.tscn")
-	
+var enemics_spawn
+var rand = RandomNumberGenerator.new()
+var enemyscene = load("res://Enemic_babosa.tscn")
+var timer
+var screen_size 
+var cooldown = 5
+var spawn = true
+var vegades = 0
+var timer_nivell
+func _ready(): 
+	timer = Timer.new()
+	add_child(timer)
+	timer.set_one_shot(true)
+	timer.set_wait_time(cooldown)
+	timer.connect("timeout",self, "_cooldown_finish")
 	var screen_size = get_viewport().get_visible_rect().size
+
+func _process(delta):
 	
-	for i in range(0,10):
-		var enemy = enemyscene.instance()
-		rand.randomize()
-	
+	if spawn == true:
+		timer.start()
+		vegades = vegades + 2 
+		enemics_spawn = vegades
+		for i in range(0,enemics_spawn):
+			var enemy = enemyscene.instance()
+			rand.randomize()
+			var x = rand.randf_range(0,1025)
+			rand.randomize()
+			var y = rand.randf_range(0,590)
+			enemy.position.y = y
+			enemy.position.x = x
+			$Enemics.add_child(enemy)
+		spawn = false
+		
+func _cooldown_finish():
+		spawn = true
